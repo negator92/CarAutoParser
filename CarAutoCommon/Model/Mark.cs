9dfcc793b3f9723a63d493c4fa443e9ae481d379
Vincent Neparidze <nva@akmetron.ru>
2019-06-20 15:27:46 +0300

    Added Avalonia MVVM template and library

---
+++ CarAutoCommon/Model/Mark.cs
@@ -0,0 +1,13 @@
+﻿namespace CarAutoCommon.Model
+{
+    /// <summary>
+    /// Manufacturer of car
+    /// </summary>
+    internal class Mark
+    {
+        /// <summary>
+        /// Name of manufacturer of car
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
